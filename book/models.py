3c7ae4b293629189594dd925bf064a40ec325704
Ephrem Wube <ephremworkeye@gmail.com>
2021-10-15 22:53:28 -0700

    author and book models added

--- book/models.py
+++ book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='book_author')
+    
